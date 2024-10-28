f552bff8095ef4faef79828591357ef6f3bd7823
Zegapaincreator <goohagis2@gmail.com>
2024-10-29 02:05:07 +0700

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -2,14 +2,9 @@ def send_email(message, recipient, *, sender = 'university.help@gmail.com'):
     recipient_ending = False
     sender_ending = False
     endings = ['.com', '.net', '.ru']
-    for i in endings:
-        if recipient.endswith(i) == True:
-            recipient_ending = True
-            break
-    for i in endings:
-        if sender.endswith(i) == True:
-            sender_ending = True
-            break
+    recipient_ending = any(recipient.endswith(i) for i in endings)
+    sender_ending = any(sender.endswith(i) for i in endings)
+
     if sender == recipient:
         print('Нельзя отправить письмо самому себе!')
     elif '@' in recipient and '@' in sender and recipient_ending and sender_ending:
@@ -19,6 +14,5 @@ def send_email(message, recipient, *, sender = 'university.help@gmail.com'):
             print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>.')
     else:
         print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>"')
-
 
 send_email('Hello', 'pochta.ru', sender='goohagis2@l.ru')
